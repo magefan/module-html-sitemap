0e8727e18a6e0f407f44b69d98e13b6b53b23d79
Bohdan Berezhniy <bohdan.berezhniy@magefan.com>
2022-04-15 18:48:26 +0300

    improved compatibility with Magefan Blog

---
+++ Setup/Recurring.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+
+namespace Magefan\HtmlSitemap\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class Recurring implements InstallSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $setup->startSetup();
+        $connection = $setup->getConnection();
+
+        foreach (['magefan_blog_post', 'magefan_blog_category'] as $tableName) {
+            $tableName = $setup->getTable($tableName);
+
+            if ($connection->isTableExists($tableName) == true && !$connection->tableColumnExists($tableName, 'mf_exclude_html_sitemap')) {
+                $columns = [
+                    'mf_exclude_html_sitemap' => [
+                        'type' => \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                        'nullable' => false,
+                        'comment' => 'Exclude HTML Sitemap',
+                        'default' => '0'
+                    ],
+                ];
+
+                foreach ($columns as $name => $definition) {
+                    $connection->addColumn($tableName, $name, $definition);
+                }
+            }
+        }
+
+        $setup->endSetup();
+    }
+}
