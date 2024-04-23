03247f26bda233174ddf63182273a04d0ed151af
Ivan Hrytsai <ivan.hrytsai@magefan.com>
2024-04-23 14:57:56 +0300

    Exclude Page

---
+++ Setup/Patch/Data/PageToExclude.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ */
+
+declare(strict_types=1);
+
+namespace Magefan\HtmlSitemap\Setup\Patch\Data;
+
+use Magento\Cms\Model\ResourceModel\Page\CollectionFactory as PageCollectionFactory;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+
+class PageToExclude implements DataPatchInterface, PatchRevertableInterface
+{
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * @var PageCollectionFactory
+     */
+    private $pageCollectionFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     * @param PageCollectionFactory $pageCollectionFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory          $eavSetupFactory,
+        PageCollectionFactory   $pageCollectionFactory
+    )
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->pageCollectionFactory = $pageCollectionFactory;
+    }
+
+    /**
+     * @return void
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        $pageCollection = $this->pageCollectionFactory->create();
+
+        $pageCollection->addFieldToFilter('identifier', ['in' => ['no-route', 'home', 'enable-cookies']]);
+
+        foreach ($pageCollection as $page) {
+            $page->setMfExcludeHtmlSitemap(1);
+        }
+
+        $pageCollection->save();
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * Revert function
+     */
+    public function revert()
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+}
+
